53140e9b6d8879a65d9417f1fe16738f2203c535
JimDeanSpivey <JimDeanSpivey@gmail.com>
2016-09-09 02:43:22 -0400

    Script to link nodes to keywords added

---
+++ data/local_keywords.sql
@@ -0,0 +1,120 @@
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3613);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3796);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3805);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3806);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3815);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3817);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3822);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3827);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3837);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3839);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3823);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3844);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3848);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3853);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3818);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3860);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3861);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3862);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3869);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3871);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3873);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3874);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3878);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3879);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3881);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3885);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3894);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3905);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3914);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3923);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3929);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3935);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3940);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3951);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3956);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3960);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3965);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3970);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3972);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3977);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3978);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3980);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3997);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 4007);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 4008);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 4014);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 4025);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 4027);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 4030);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 4031);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3053);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 3913);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 4052);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 4053);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 4058);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 4061);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 4068);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 4076);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 4084);
+insert into keyword_twitter_api_node (api_node_id, keyword_id) values
+  (1, 4088);
